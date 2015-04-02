e8e2440094c5ce8bbccf52d204dd6bc448bc04a1
Shishir Mahajan <shishir.mahajan@redhat.com>
2015-04-02 16:16:43 -0400

    Change Node fields {data, next} from unexported to exported fields
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

--- src/linkedList/ll.go
+++ src/linkedList/ll.go
@@ -3,14 +3,14 @@ package linkedList
 import "fmt"
 
 type Node struct {
-	data int
-	next *Node
+	Data int
+	Next *Node
 }
 
 func createNode(data int, head *Node) *Node {
 	head = new(Node)
-	head.data = data
-	head.next = nil
+	head.Data = data
+	head.Next = nil
 	return head
 }
 
@@ -18,17 +18,17 @@ func AppendNode(data int, head *Node) *Node {
 	if head == nil {
 		head = createNode(data, head)
 	} else {
-		head.next = new(Node)
-		head = head.next
-		head.data = data
-		head.next = nil
+		head.Next = new(Node)
+		head = head.Next
+		head.Data = data
+		head.Next = nil
 	}
 	return head
 }
 
 func PrintLinkedList(head *Node) {
 	for head != nil {
-		fmt.Println(head.data)
-		head = head.next
+		fmt.Println(head.Data)
+		head = head.Next
 	}
 }
