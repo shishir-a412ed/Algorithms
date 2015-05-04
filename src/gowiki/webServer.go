16f0817fd000b3032915158a140bb22ca19da44a
Shishir Mahajan <shishir.mahajan@redhat.com>
2015-05-04 17:06:49 -0400

    Using the webServer to handle gowiki pages
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

--- src/gowiki/webServer.go
+++ src/gowiki/webServer.go
@@ -6,15 +6,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"gowiki/wiki"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi There, I love %s!", r.URL.Path[1:])
+func printHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi There, I love %s!", r.URL.Path[len("/print/"):])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request){
+
+	title := r.URL.Path[len("/view/"):]
+	p, _ := wiki.LoadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/print/", printHandler)
+	http.HandleFunc("/view/",viewHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalln(err)
 	}
+
 }
---
+++ src/gowiki/wiki/wiki.go
@@ -0,0 +1,24 @@
+package wiki
+
+import (
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) Save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func LoadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
