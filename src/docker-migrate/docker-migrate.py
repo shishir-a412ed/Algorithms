795d66044d047bfc6e8b0613eaaeab20ba09acf7
Shishir Mahajan <shishir.mahajan@redhat.com>
2015-10-06 14:11:03 -0400

    Adding forward declaration, restructuring and modularizing docker-migrate code
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

--- src/docker-migrate/docker-migrate.py
+++ src/docker-migrate/docker-migrate.py
@@ -1,23 +1,58 @@
 #!/usr/bin/env python
 
-# porting docker-migrate.sh to python
 import sys
 import os
 import subprocess
 
-if len(sys.argv) == 3:
-    if sys.argv[1] == "export":
-        if not os.path.isdir(sys.argv[2]):
-            os.mkdir(sys.argv[2])
-        if not os.path.isdir(sys.argv[2] + "/images"):
-            os.mkdir(sys.argv[2] + "/images")
-        if not os.path.isdir(sys.argv[2] + "/containers"):
-            os.mkdir(sys.argv[2] + "/containers")
-        if not os.path.isdir(sys.argv[2] + "/volumes"):
-            os.mkdir(sys.argv[2] + "/volumes")
-        # The following is some rather hacky list operations so that we
-        # can save images by name:tags instead of by ID
-        images = subprocess.check_output("sudo docker images -a", shell=True)
+
+def main():
+	if len(sys.argv) == 1:
+		show_help()
+	elif len(sys.argv) == 2 and sys.argv[1] == "help":
+		show_help()
+
+	if os.geteuid() != 0:
+                exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+
+	if len(sys.argv) == 2 and sys.argv[1] == "export":
+		exportDir = "/var/lib/docker-migrate"
+		export_docker(exportDir)		
+	elif len(sys.argv) == 3 and sys.argv[1] == "export":
+		exportDir = sys.argv[2]
+		export_docker(exportDir)
+	elif len(sys.argv) == 2 and sys.argv[1] == "import":
+                importDir = "/var/lib/docker-migrate"
+                import_docker(importDir)
+        elif len(sys.argv) == 3 and sys.argv[1] == "import":
+                importDir = sys.argv[2]
+                import_docker(importDir)	
+		
+def export_docker(exportDir):
+	if not os.path.isdir(exportDir):
+		os.mkdir(exportDir)
+	#export docker images
+	export_images(exportDir)
+	#export docker containers
+	export_containers(exportDir)
+	#export docker volumes
+	export_volumes(exportDir)
+	print("docker export completed successfully")
+
+def import_docker(importDir):
+        if not os.path.isdir(importDir):
+        	sys.exit("Specified directory {0} does not exist".format(importDir))
+	#import docker images
+        import_images(importDir)
+        #import docker containers
+        import_containers(importDir)
+        #import docker volumes
+        import_volumes(importDir)
+	print("docker import completed successfully")
+
+def export_images(exportDir):
+	if not os.path.isdir(exportDir + "/images"):
+        	os.mkdir(exportDir + "/images")
+	images = subprocess.check_output("docker images", shell=True)
         splitImages = images.split()[7:]  # cut off the headers
         names = []
         tags = []
@@ -32,64 +67,79 @@ if len(sys.argv) == 3:
             print("Saving image {0}:{1}".format(names[i], tags[i]))
             if names[i] == '<none>':
                 subprocess.call(
-                    "sudo docker save {0} > {1}/images/{0}-{0}.tar".format(
-                        ids[i], sys.argv[2]), shell=True)
+                    "docker save {0} > {1}/images/{0}-{0}.tar".format(
+                        ids[i], exportDir), shell=True)
             else:
                 subprocess.call(
-                    "sudo docker save {0}:{1} > {2}/images/{3}-{4}.tar".format(
-                        names[i], tags[i], sys.argv[2], names[i].replace("/", "~"), tags[i].replace("/", "~")), shell=True)
-        subprocess.call(
-            "sudo tar -zcvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes . > /dev/null".format(sys.argv[2]), shell=True)
-        if os.path.isdir("/var/lib/docker/vfs"):
-            subprocess.call("sudo tar -zcvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs . > /dev/null".format(sys.argv[2]), shell=True)
+                    "docker save {0}:{1} > {2}/images/{3}-{4}.tar".format(
+                      	names[i], tags[i], exportDir, names[i].replace("/", "~"), tags[i].replace("/", "~")), shell=True)
 
-    elif sys.argv[1] == "import":
-        if not os.path.isdir(sys.argv[2]):
-            sys.exit("Specified directory {0} does not exist".format(sys.argv[2]))
-        tarballs = subprocess.check_output("ls {0}/images".format(sys.argv[2]), shell=True)
+def export_containers(exportDir):
+	if not os.path.isdir(exportDir + "/containers"):
+            os.mkdir(exportDir + "/containers")
+
+def export_volumes(exportDir):
+	if not os.path.isdir(exportDir + "/volumes"):
+            os.mkdir(exportDir + "/volumes")
+	subprocess.call(
+            "tar -zcvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes . > /dev/null".format(exportDir), shell=True)
+        if os.path.isdir("/var/lib/docker/vfs"):
+            subprocess.call("tar -zcvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs . > /dev/null".format(exportDir), shell=True)
+
+def import_images(importDir):
+	tarballs = subprocess.check_output("ls {0}/images".format(importDir), shell=True)
         splitTarballs = tarballs.split()
         for i in splitTarballs:
             print("Loading image {0}".format(i))
-            subprocess.call("sudo docker load < {0}/images/{1}".format(sys.argv[2], i), shell=True)
-        subprocess.call(
-            "sudo tar xzvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes > /dev/null".format(sys.argv[2]), shell=True)
+            subprocess.call("docker load < {0}/images/{1}".format(importDir, i), shell=True)
+
+def import_containers(importDir):
+	print("import_containers")
+
+def import_volumes(importDir):
+	subprocess.call(
+            "tar xzvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes > /dev/null".format(importDir), shell=True)
         if os.path.isdir("/var/lib/docker/vfs"):
             subprocess.call(
-                "sudo tar -xzvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs > /dev/null".format(sys.argv[2]), shell=True)
-        print("If you created directory {0} solely for the purpose of temporary storage for your files during the docker-migrate import/export process, you may now remove it if you so desire".format(sys.argv[2]))
-    else:
-        print("Please specify an option: 'import', 'export', or 'help' and try again")
+                "tar -xzvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs > /dev/null".format(importDir), shell=True)
 
-elif len(sys.argv) == 2:
-    if (sys.argv[1] == "help"):
-        sys.exit("""
-# Docker-Migrate
+def show_help():
+	os.system("clear")
+	sys.exit("""
 
-This script allows the user to easily migrate images, volumes, and
-containers from one version of Docker to another.  With the script, users can quickly save all their data from the current docker
-instance, change the docker storage backend, and then import all their
-old data to the new system.
+	DOCKER MIGRATE
 
-## ./docker-migrate export [directory]
+	This tool allows the user to easily migrate images, volumes, and
+        containers from one version of Docker to another. With this tool, 
+        users can quickly save all their data from the current docker
+        instance, change the docker storage backend, and then import all 
+        their old data to the new system.
 
-Specify the directory in which to temporarily store the files (can be
-an existing directory, or the command will create one)
+        ## ./docker-migrate export [directory]
 
-The export command will export all the current images, volumes, and
-containers to the specified directory, in the /images, /volumes,
-/containers subdirectories.
+        Specify the directory in which to temporarily store the files (can be
+        an existing directory, or the command will create one). If no directory
+        is specified, `/var/lib/docker-migrate` would be used as default.
 
-## ./docker-migrate import [directory]
+        The export command will export all the current images, volumes, and
+        containers to the specified directory, in the /images, /volumes,
+        /containers subdirectories.
 
-Specify the directory from which to read the files (must be an
-existing directory)
+        ## ./docker-migrate import [directory]
 
-The import command will import images, volumes, and containers from
-the specified directory into the new docker instance.
+        Specify the directory from which to read the files (must be an
+        existing directory).If no directory is specified, 
+        `/var/lib/docker-migrate` would be used as default.
 
-Jenny Ramseyer, 2015
+        The import command will import images, volumes, and containers from
+        the specified directory into the new docker instance.
+
+        Primary Author: Jenny Ramseyer, 2015
+        Secondary Author: Shishir Mahajan, 2015
 """)
-    else:
-        print("Please specify an option: 'import', 'export', or 'help' and try again")
-else:
-    print("Please specify an option: 'import', 'export', or 'help' and try again")
+
+main()
+
+
+
+	
